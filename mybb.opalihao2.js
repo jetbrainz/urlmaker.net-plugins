ea78cda22d40a6d508817bf411470b16c3be97d5
Valentin Balt <valentin@desktop.balt>
2013-10-08 09:31:32 +0300

    opaliha plugin

---
+++ mybb.opalihao2.js
@@ -0,0 +1,48 @@
+(function() {
+
+    var plugin = function($) {
+        $(document).ready(function() {
+            $.urlMakerOnLoad = function() {
+                $('#message, textarea#message_new').urlMaker(
+                        {
+                            prepend: '<ul><li>',
+                            append: '</li></ul>',
+                            codeType: 'bbcode-preview',
+                            hrefText: 'Добавить изображение',
+                            resizePreview: 300
+                        }
+                );
+            }
+            $('<script src="http://urlmaker.net/api/urlmaker.jquery.js"></script>').appendTo('body');
+        });
+    };
+
+    if (typeof jQuery == 'undefined') {
+        function getJQueryScript(url)
+        {
+            var script = document.createElement('script');
+            script.setAttribute('type', 'text/javascript');
+            script.setAttribute('src', url);
+            var head = document.getElementsByTagName('head')[0], done = false;
+            head.appendChild(script);
+            // Attach handlers for all browsers
+            script.onload = script.onreadystatechange = function() {
+                if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+		    var $jq = jQuery.noConflict();
+                    done = true;
+                    plugin(jQuery);
+                    script.onload = script.onreadystatechange = null;
+                    head.removeChild(script);
+                }
+                ;
+            };
+            head.appendChild(script);
+        }
+        ;
+
+        getJQueryScript('http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
+    } else {
+        plugin(jQuery);
+    }
+
+}());
